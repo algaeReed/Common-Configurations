e9400be96ffb364dc8edb321efa9101a22c84c36
algae Reed <30750777+algaeReed@users.noreply.github.com>
2024-06-18 22:22:53 +0800

    Create babel.config.js

---
+++ react-native-alias-config/alias/babel.config.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@src': './src',
+        },
+      },
+    ],
+  ],
+  overrides: [
+    {
+      test: [
+        "./src/RmAdManager/index.ts"
+      ],
+      plugins: ["babel-plugin-transform-remove-console"]
+    }
+  ]
+};
